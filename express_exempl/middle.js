eb7460b57816cbfaa10564587dd52b8861c55712
rerribe <rribeirocoder@gmail.com>
2025-05-08 13:46:02 -0300

    teste
    teste dia 08

---
+++ express_exempl/middle.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const path = require('path');
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '/index.html'));
+})
+
+app.get('/hello', (req, res) => {
+    res.sendFile(path.join(__dirname, '/hello.html'));
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
